59822e39c92d71edda5fd54dd6dad0c41f013de2
DaBlower <113282214+obobed@users.noreply.github.com>
2026-07-07 01:15:47 +0800

    fix things

--- minimal/minimal.ino
+++ minimal/minimal.ino
@@ -3,19 +3,11 @@
 void setup() {
   pinMode(1, OUTPUT);
   pinMode(2, OUTPUT);
-  digitalWrite(1, HIGH);
-  digitalWrite(2, LOW);
   ledcAttach(SERVO_PIN, 50, 16);
-}
-
-void setAngle(int angle) {
-  int pulseWidth = map(angle, 0, 180, 500, 2500);
-  ledcWrite(SERVO_PIN, pulseWidth * 65536 / 20000);
+  digitalWrite(1, LOW);
+  digitalWrite(2, HIGH);
+  ledcWrite(SERVO_PIN, 32768);
 }
 
 void loop() {
-  setAngle(90);
-  delay(1000);
-  setAngle(0);
-  delay(1000);
 }
