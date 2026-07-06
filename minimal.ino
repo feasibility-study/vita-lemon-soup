13578ad33160ce8cd92c686e499e4aa2aefb421e
DaBlower <113282214+obobed@users.noreply.github.com>
2026-07-07 03:31:02 +0800

    idk maybe minimal

--- minimal.ino
+++ minimal.ino
@@ -1,26 +1,35 @@
-#include <Servo.h>
+#define SERVO_PIN 44
 
-Servo servo;
+void servoAngle(int angle) {
+  int pulseWidth = map(angle, 0, 180, 500, 2500);
+  ledcWrite(SERVO_PIN, pulseWidth * 65536 / 20000);
+}
 
 void setup() {
   pinMode(1, OUTPUT);
   pinMode(2, OUTPUT);
-  servo.attach(44);
+  ledcAttach(SERVO_PIN, 50, 16);
 
   
-    servo.write(0);
-    digitalWrite(1, HIGH);
-    digitalWrite(2, LOW);
-    delay(5000);
-    servo.write(90);
-    digitalWrite(1, LOW);
-    digitalWrite(2, LOW);
-    delay(2000);
-    digitalWrite(1, LOW);
-    digitalWrite(2, HIGH);
-    delay(10000);
+  servoAngle(0);
+  digitalWrite(1, HIGH);
+  digitalWrite(2, LOW);
+  delay(5000);
+
+  servoAngle(90);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  delay(2000);
+
+  ledcWrite(SERVO_PIN, 32768);
+  digitalWrite(1, LOW);
+  digitalWrite(2, HIGH);
+  delay(10000);
+
+  servoAngle(90);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
 }
 
 void loop() {
-
 }
--- minimal/minimal.ino
+++ minimal/minimal.ino
@@ -1,12 +1,45 @@
 #define SERVO_PIN 44
 
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
+
+#define SERVOMIN 125
+#define SERVOMAX 625
+
+
+void servoAngle(int angle) {
+  int pulseWidth = map(angle, 0, 180, 500, 2500);
+  ledcWrite(SERVO_PIN, pulseWidth * 65536 / 20000);
+}
+
 void setup() {
+  pwm.begin();
+  pwm.setPWMFreq(50);
+
   pinMode(1, OUTPUT);
   pinMode(2, OUTPUT);
   ledcAttach(SERVO_PIN, 50, 16);
+  
+  pwm.setPWM(0, 0, 0);
+  digitalWrite(1, HIGH);
+  digitalWrite(2, LOW);
+  delay(5000);
+
+  pwm.setPWM(0, 0, 90);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  delay(2000);
+
+  ledcWrite(SERVO_PIN, 32768);
   digitalWrite(1, LOW);
   digitalWrite(2, HIGH);
-  ledcWrite(SERVO_PIN, 32768);
+  delay(10000);
+
+  pwm.setPWM(0, 0, 0);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
 }
 
 void loop() {
